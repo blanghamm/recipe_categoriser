596f3d3139ca87deae42aba08ea718d7ebd3e474
blanghamm <langham1291@outlook.com>
2023-01-12 22:52:50 +0000

    Added: sizing measurement

--- scraper/bbcfood.py
+++ scraper/bbcfood.py
@@ -30,13 +30,15 @@ def get_quantity_and_measurement(value):
     float_matches = re.findall(r'(\d+\.\d+)', value)
     tsp = re.findall(r'(\btsp\b)', value)
     tbsp = re.findall(r'(\btbsp\b)', value)
+    large = re.findall(r'(\blarge\b)', value)
+    medium = re.findall(r'(\bmedium\b)', value)
+    small = re.findall(r'(\bsmall\b)', value)
     symbol_half = re.findall(r'(\½)', value)
     symbol_quarter = re.findall(r'(\¼)', value)
     symbol_three_quarter = re.findall(r'(\¾)', value)
     amount = int_single + float_matches + symbol_half + symbol_quarter + symbol_three_quarter
-    measurement_type = grams + tsp + tbsp + ml
-    print(amount, measurement_type)
-    return {"amount": amount, "measurement": measurement_type}
+    measurement_type = grams + tsp + tbsp + ml + large + medium + small
+    return {"amount": None if len(amount) == 0 else _num(amount[0]), "measurement": None if len(measurement_type) == 0 else measurement_type[0]}
 
 
 def _num(s):
